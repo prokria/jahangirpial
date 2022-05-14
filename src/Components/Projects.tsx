ba3843c0e671ea850ab45402b788bbd5b162ca93
Jahangir Pial <jahangiralompial@gmail.com>
2022-05-14 16:22:23 +0600

    First Release

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Grid";
@@ -37,10 +35,6 @@ export default function Projects() {
                   content.
                 </Typography>
               </CardContent>
-              <CardActions>
-                <Button size="small">View</Button>
-                <Button size="small">Edit</Button>
-              </CardActions>
             </Card>
           </Grid>
         ))}
--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -6,9 +6,10 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Footer from "../Components/Footer";
-import Header from "../Components/Header";
-import Projects from "../Components/Projects";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const theme = createTheme();
 
@@ -16,35 +17,32 @@ export default function LandingPage() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
       <main>
         {/* Hero unit */}
         <Box
           sx={{
-            bgcolor: "background.paper",
+            bgcolor: "#f7f1e3",
             pt: 8,
             pb: 6,
+            minHeight: "100vh",
           }}
         >
           <Container maxWidth="sm">
+            <Box sx={{ textAlign: "center", padding: 3 }}>
+              <img src="/logo.png" alt="JahangirPial" width="200" />
+            </Box>
+
             <Typography
-              component="h1"
-              variant="h2"
+              variant="subtitle1"
               align="center"
               color="text.primary"
-              gutterBottom
-            >
-              Jahangir Pial
-            </Typography>
-            <Typography
-              variant="h5"
-              align="center"
-              color="text.secondary"
               paragraph
             >
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don&apos;t simply skip over it entirely.
+              I'm <strong>Jahangir Pial</strong> a professional Software
+              Engineer, currently working at mPower Social. Also, I'm a full
+              time learner of new technologies and many more things! If you have
+              any investment idea or plan to develop any tech project, feel free
+              to contact with me.
             </Typography>
             <Stack
               sx={{ pt: 4 }}
@@ -52,16 +50,37 @@ export default function LandingPage() {
               spacing={2}
               justifyContent="center"
             >
-              <Button variant="contained">Main call to action</Button>
-              <Button variant="outlined">Secondary action</Button>
+              <Button
+                variant="outlined"
+                href="https://www.facebook.com/jpial/"
+                target="_blank"
+              >
+                <FacebookIcon />
+              </Button>
+              <Button
+                variant="outlined"
+                href="https://twitter.com/jahangirpial"
+                target="_blank"
+              >
+                <TwitterIcon />
+              </Button>
+              <Button
+                variant="outlined"
+                href="https://github.com/japial"
+                target="_blank"
+              >
+                <GitHubIcon />
+              </Button>
+              <Button
+                variant="outlined"
+                href="mailto:jahangiralompial@gmail.com"
+              >
+                <EmailIcon />
+              </Button>
             </Stack>
           </Container>
         </Box>
-        <Projects />
       </main>
-      {/* Footer */}
-      <Footer />
-      {/* End footer */}
     </ThemeProvider>
   );
 }
